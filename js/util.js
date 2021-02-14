122fd23660aa108c4c91a5b9fc606c3ea6948f9e
Alesya <kyzya77777@gmail.com>
2021-02-14 13:51:54 +0300

    Залание 4. Разделила код на модули

---
+++ js/util.js
@@ -0,0 +1,32 @@
+const getRandomInteger = (rangeFrom, rangeTo) => {
+  if (rangeFrom >=0 && rangeTo >=0) {
+    if (rangeFrom < rangeTo) {
+      rangeFrom = Math.ceil(rangeFrom);
+      rangeTo = Math.floor(rangeTo);
+      return Math.floor(Math.random() * (rangeTo - rangeFrom )) + rangeFrom;
+    } else {
+      throw new Error('Задан не верный диапазон. Значение от не может быть больше значения до. Задайте правильный диапазон.');
+    }
+  } else {
+    throw new Error('Задан отрицательный диапазон');
+  }
+}
+
+getRandomInteger (2, 6);
+
+
+const getRandomFloatNumber = (rangeFrom, rangeTo, precision = 9) => {
+  if (rangeFrom >=0 && rangeTo >=0) {
+    if (rangeFrom < rangeTo) {
+      return parseFloat((Math.random() * (rangeTo - rangeFrom ) + rangeFrom).toFixed(precision));
+    } else {
+      throw new Error('Задан не верный диапазон. Значение от не может быть больше значения до. Задайте правильный диапазон.');
+    }
+  } else {
+    throw new Error('Задан отрицательный диапазон');
+  }
+}
+
+getRandomFloatNumber (1.2, 5.6);
+
+export {getRandomInteger, getRandomFloatNumber};
